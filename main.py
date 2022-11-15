cfdedd71cf4c8dbf0db7921ab59ae65297778562
ilvesBogdan <116699847+ilvesBogdan@users.noreply.github.com>
2022-11-15 14:26:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-def qestion1(*args: int) -> int:
+def question1(*args: int) -> int:
     """Решение задания 1"""
     return sum(args) // len(args)
 
 
-def qestion2(x: int, y: int) -> int:
+def question2(x: int, y: int) -> int:
     """Решение задания 2"""
     return x // y, x % y
 
 
-def qestion3(r: int, x: float) -> float|int:
+def question3(r: int, x: float) -> float|int:
     """Решение задания 3"""
     if r < 0:
         raise Exception("Первый аргумент должен быть больше или равен нулю.")
@@ -18,12 +18,12 @@ def qestion3(r: int, x: float) -> float|int:
         return int(x)
 
 
-def qestion4(x: int) -> int:
+def question4(x: int) -> int:
     """Решение задания 4"""
-    return int(str(x)[::-1])
+    return int(str(abs(x))[::-1])
 
 
-def qestion5(x: int) -> int:
+def question5(x: int) -> int:
     """Решение задания 5"""
     if 32 < x.__sizeof__():
         return 0
@@ -31,8 +31,8 @@ def qestion5(x: int) -> int:
 
 
 if __name__ == '__main__':
-    print('\n Задание 1:', qestion1(52, 56, 60))
-    print('\n Задание 2:', qestion2(9, 2))
-    print('\n Задание 3:', qestion3(2, 14.721))
-    print('\n Задание 4:', qestion4(123))
-    print('\n Задание 5:', qestion5(1245163476234786782))
+    print('\n Задание 1:', question1(52, 56, 60))
+    print('\n Задание 2:', question2(9, 2))
+    print('\n Задание 3:', question3(2, 14.721))
+    print('\n Задание 4:', question4(123))
+    print('\n Задание 5:', question5(1245163476234786782))
